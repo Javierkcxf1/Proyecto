fd165775fa2b28a3a5c189eddd7420e64123b865
Javier <javiertorresmvm@gmail.com>
2025-03-03 19:35:05 -0500

    Tienda de discos

--- TiendaDeDiscos/obj/Debug/net9.0/TiendaDeDiscos.AssemblyInfo.cs
+++ TiendaDeDiscos/obj/Debug/net9.0/TiendaDeDiscos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TiendaDeDiscos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bfa4eee1077e05edc5e4ae9eb7743b4cb63b771")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+229fcce52865cea0b6753f783ab25e018aa4e53a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TiendaDeDiscos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TiendaDeDiscos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
